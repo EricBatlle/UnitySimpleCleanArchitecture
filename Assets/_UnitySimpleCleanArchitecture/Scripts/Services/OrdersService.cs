5a31ae178176c8bdf136cf3e210f34cea34d0397
Eric <erikbc_95@hotmail.com>
2024-03-03 17:17:51 +0100

    Move OrdersService to its own file

---
+++ Assets/_UnitySimpleCleanArchitecture/Scripts/Services/OrdersService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public class OrdersService
+{
+	private readonly OrdersViewSpawner ordersViewSpawner;
+	private readonly OrderRandomGenerator orderRandomGenerator;
+
+	private readonly List<OrderView> ordersViews = new List<OrderView>();
+
+	public OrdersService(OrdersViewSpawner ordersViewSpawner, OrderRandomGenerator orderRandomGenerator)
+	{
+		this.ordersViewSpawner = ordersViewSpawner;
+		this.orderRandomGenerator = orderRandomGenerator;
+	}
+
+	public void CreateOrder(int maxIngredientsPerOrder)
+	{
+		var order = orderRandomGenerator.Generate(maxIngredientsPerOrder);
+		var orderView = ordersViewSpawner.Spawn(order);
+		ordersViews.Add(orderView);
+	}
+}
